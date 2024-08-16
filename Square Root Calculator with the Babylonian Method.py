6a27e7157a9f7a00dcaf4af2540279b0fd50fcbc
Lucas Romero Fernández <lucansio.romero14@gmail.com>
2024-08-16 13:33:26 +0200

    Update Square Root Calculator with the Babylonian Method.py

--- Square Root Calculator with the Babylonian Method.py
+++ Square Root Calculator with the Babylonian Method.py
@@ -5,9 +5,12 @@ Square Root Calculator with the Babylonian Method
 @author: Lucas Romero Fernández
 """
 from timeit import timeit
+import time
+import numpy as np
 #More information about the method in this website: https://blogs.sas.com/content/iml/2016/05/16/babylonian-square-roots.html
 #main_program
-def square_root(x):
+start_time_program=time.process_time()#To calculate the program execution time
+def square_root_BM(x):#Babylonian method
     if x<0:
         x=-x
         v=1j
@@ -19,10 +22,40 @@ def square_root(x):
         t=r
         r=1/2*((x/r)+r)
     return r*v
+def square_root_SQRT(x):#sqrt method
+    return np.emath.sqrt(n)
+#For the timeit module
+square_root_BM_string='''
+def square_root_BM(x):
+    if x<0:
+        x=-x
+        v=1j
+    else:
+        v=1
+    r=x
+    t=0
+    while t!=r:
+        t=r
+        r=1/2*((x/r)+r)
+    return r*v
+    '''
+square_root_SQRT_string='''
+def square_root_SQRT(x):
+    return np.emath.sqrt(n)
+    '''
+#Results
 n=float(input("Number? "))
 print("")
-print("The square root of ",n," is ",square_root(n),".",sep="")
+print("With the babilonian method, the square root of ",n," is ",square_root_BM(n),".",sep="")
 print("")
-print("Program execution time:",timeit(number=1),"seconds.")
+print("Execution time of the babilonian method:",timeit(square_root_BM_string,number=1),"seconds.")
+print("")
+print("With the built-in numpy function emath.sqrt, the square root of ",n," is ",square_root_SQRT(n),".",sep="")
+print("")
+print("Execution time of the numpy function emath.sqrt:",timeit(square_root_SQRT_string,"import numpy as np",number=1),"seconds.")
+print("")
+print("Value of the error between methods =",abs(square_root_SQRT(n)-square_root_BM(n)))
+print("")
+print("Program execution time:",time.process_time()-start_time_program,"seconds.")
 print("")
 print("Goodbye.")
