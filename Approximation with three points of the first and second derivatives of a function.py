30b5c0ffef67ce70da9e0df1d453a8bce6ace0db
Lucas Romero Fernández <lucansio.romero14@gmail.com>
2025-08-14 12:53:46 +0200

    Add files via upload

--- Approximation with three points of the first and second derivatives of a function.py
+++ Approximation with three points of the first and second derivatives of a function.py
@@ -1,45 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct  2 20:10:35 2021
-Approximation with three points of the first and second derivatives of a function at a point x0=0 (numerical differentiation with finite differences).
-It is assumed that the function fits perfectly to the second order polynomial function ax^2+bx+c that goes through the three points.
+Created on Sat Oct 2 20:10:35 2021
+Approximation with three points of the first and second derivatives of a function f(x) at a point x0=0 (numerical differentiation with finite differences).
 More information on this website: https://en.wikipedia.org/wiki/Numerical_differentiation
 @author: Lucas Romero Fernández
 """
 from sympy import *
 #main_program
-#Definition of lists and constants
+#Definition of the constants and function
 x0=0
-h=1#Value of the real and positive step size, must match the differences between the x values
-#Main process of the analytical obtainment of the first and second derivatives of a function at a point x0
-def pol_2order_an():
-    x=Symbol("x")
-    a,b,c=symbols("a,b,c")
-    d1=diff(a*x**(2)+b*x+c,x)
-    d1_0=N(d1.subs(x,x0))
-    d2=diff(d1,x)
-    d2_0=N(d2.subs(x,x0))
-    print("Analitically, the first derivative of the second order polynomial function at the point x0 =",x0,"is equal to",d1_0,"and the second derivative of the same function at that same point is equal to",d2_0)
-    print("")
-pol_2order_an()
-#Main process of the numerical obtainment of the first and second derivatives of a function at a point x0
-a=float(input("Value of a: "))
-print("")
-b=float(input("Value of b: "))
-print("")
-c=float(input("Value of c: "))#As can be seen, this value does not affect the derivatives. Left here so it can be verified personally
-print("")
-def pol_2order_ex():
-    x=Symbol("x")
-    d1=diff(a*x**(2)+b*x+c,x)
+h=0.1#Value of the real and positive step size. Decrease it to improve accuracy.
+x=Symbol("x")
+def f(x):#Note: If a function like ln(x),e^(x),sqrt(x),etc. is put here, be sure to use the version of SymPy to avoid errors in the expression of the equation.
+    return sin(x)
+#Main process of the ''exact'' numerical obtainment of the first and second derivatives of a function at a point x0
+def diff_func_ex():
+    d1=diff(f(x),x)
     d1_0=N(d1.subs(x,x0))
     d2=diff(d1)
     d2_0=N(d2.subs(x,x0))
     return d1_0,d2_0
-def pol_2order_cal(x):#For the calculus of the approximations
-    return a*x**(2)+b*x+c
+#Main process of the ''approximated'' numerical obtainment of the first and second derivatives of a function at a point x0
+def diff_func_approx():
+    d1_0=(f(x0+h)-f(x0-h))/(2*h)
+    d2_0=(f(x0+h)-2*f(0)+f(x0-h))/(h**2)
+    return d1_0,d2_0
 #Results
-print("The values of the first and second derivatives of the second order polynomial function calculated analitically at the point x0 =",x0,"is, respectively,",float(pol_2order_ex()[0]),"and",float(pol_2order_ex()[1]))
+print("The ''exact'' values of the first and second derivatives of the function",f(x),"calculated numerically at the point x0 =",x0,"is, respectively,",float(diff_func_ex()[0]),"and",float(diff_func_ex()[1]))
 print("")
-print("The approximated values of the first and second derivatives of the second order polynomial function calculated numerically at the point x0 =",x0,"is, respectively,",(pol_2order_cal(1)-pol_2order_cal(-1))/(2*h),"and",(pol_2order_cal(1)-2*pol_2order_cal(0)+pol_2order_cal(-1))/(h**2))
-
+print("The ''approximated'' values of the first and second derivatives of the function",f(x),"calculated numerically at the point x0 =",x0,"is, respectively,",float(diff_func_approx()[0]),"and",float(diff_func_approx()[1]))
