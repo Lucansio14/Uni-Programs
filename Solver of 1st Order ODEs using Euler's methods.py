d278af96434481d4c6f0de821d023f670d67eb08
Lucas Romero Fernández <lucansio.romero14@gmail.com>
2025-09-24 13:50:25 +0200

    Update Solver of 1st Order ODEs using Euler's methods.py

--- Solver of 1st Order ODEs using Euler's methods.py
+++ Solver of 1st Order ODEs using Euler's methods.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 18 20:20:34 2021
-Solver of 1st Order Ordinary Differential Equations (ODEs): y'(x) = f(x,y) using Euler's different methods with different step sizes "h" in the region [x0,xf] in the x-axis with initial condition y0=y(0).
-More information on this websites: https://en.wikipedia.org/wiki/Euler_method#:~:text=The%20Euler%20method%20is%20a,proportional%20to%20the%20step%20size.
-                                   https://www.maths.tcd.ie/~ryan/teaching/11404/March18_20_HigherOrderMethods.pdf
-                                   https://math.libretexts.org/Courses/Monroe_Community_College/MTH_225_Differential_Equations/03%3A_Numerical_Methods/3.02%3A_The_Improved_Euler_Method_and_Related_Methods
+Solver of 1st Order Ordinary Differential Equations (ODEs): y'(x) = f(x,y) using Euler's different methods with different step sizes "h" in the region [x0,xf] in the x-axis with initial condition y0=y(x0).
+More information on these websites: https://en.wikipedia.org/wiki/Euler_method#:~:text=The%20Euler%20method%20is%20a,proportional%20to%20the%20step%20size.
+                                    https://www.maths.tcd.ie/~ryan/teaching/11404/March18_20_HigherOrderMethods.pdf
+                                    https://math.libretexts.org/Courses/Monroe_Community_College/MTH_225_Differential_Equations/03%3A_Numerical_Methods/3.02%3A_The_Improved_Euler_Method_and_Related_Methods
 @author: Lucas Romero Fernández
 """
 import time
@@ -90,4 +90,5 @@ for h in h_list:
     plt.tight_layout()
     plt.show()
     n+=1
+
 print("Program execution time:",time.process_time()-start_time_program,"seconds.")
